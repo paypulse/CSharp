27765343535ad143af31c8efd6262200d276a35f
paypulse <sourcedev5@gmail.com>
2018-06-11 20:10:14 +0900

    20180611_firstExample

---
+++ ReflectionEx/ReflectionEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
